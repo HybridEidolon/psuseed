40518790843fe21cbaa37c6248ae718a6c4b0cf5
Ronald Kinard <furyhunter600@gmail.com>
2016-12-14 01:12:16 -0600

    msvc instead also cool stuff

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ proof of concept because i prefer writing stuff in rust rather than C++
 
 ## what do
 
-    cargo rustc -- -C link-args="dinput8.def"
+use nightly rust msvc (**ONLY MSVC NOT GNU**) and cargo build
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,41 @@
 extern crate winapi;
+extern crate kernel32;
+extern crate libc;
 //#[macro_use] extern crate minhook;
+
+use std::mem;
 
 use std::fs::File;
 use std::io::Write;
+use std::ffi::{CStr, CString};
+
+use winapi::*;
+use winapi::minwindef::*;
+use winapi::guiddef::*;
+use winapi::unknwnbase::*;
+use kernel32::*;
 
 #[allow(non_snake_case)]
 //#[no_mangle]
 #[export_name = "DirectInput8Create"]
-pub extern "stdcall" fn DirectInput8Create(_hinstance: i32, _version: i32, _riid: *const i32, _out: *mut *mut i32, _u: i32) -> i32 {
+pub extern "stdcall" fn DirectInput8Create(inst: HINSTANCE, version: DWORD, _riid: *const IID, _out: *mut LPVOID, _u: LPUNKNOWN) -> i32 {
     let mut f = File::create("foo.txt").unwrap();
     let a: Option<isize> = None;
-    writeln!(f, "lol it opened {:?}", a).unwrap();
+    writeln!(f, "lol it opened {:?}\n", a).unwrap();
+
+    let syspath = unsafe {
+        let mut syspath: [u8; MAX_PATH] = [0; MAX_PATH];
+        let syspath_ptr: LPSTR = mem::transmute(&mut syspath);
+        GetSystemDirectoryA(syspath_ptr, MAX_PATH as u32);
+        writeln!(f, "called GetSystemDirectoryA\n").unwrap();
+        match CStr::from_bytes_with_nul(&syspath[..]) {
+            Ok(c) => c.to_owned(),
+            Err(e) => {
+                writeln!(f, "error making cstring: {:?}\n", e).unwrap();
+                CString::new("C:\\Windows\\system32\\").unwrap()
+            }
+        }
+    };
+    writeln!(f, "{}\n", syspath.to_string_lossy()).unwrap();
     1
 }
